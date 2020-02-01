28c46544a55e023d2cf8075858743c232c22a486
Luiz Pires <luiz@kw13.com.br>
2020-02-01 17:16:13 -0300

    refactor: move all signup protocols to a single file

---
+++ src/presentention/controllers/signup/signup-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols' // protocoloes genericos
+export * from '../../protocols/email-validator' // Protocolos especificos
+export * from '../../../domain/usecases/add-account' // protocolos externos
+export * from '../../../domain/models/account' // protocolos externos
