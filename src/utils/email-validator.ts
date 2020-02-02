79208c12c868e263c6b88e13062a34f735db7e3c
Luiz Pires <luiz@kw13.com.br>
2020-02-01 22:34:24 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,7 @@
+import { EmailValidator } from '../presentention/protocols/email-validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  public isValid (email: string): boolean {
+    return false
+  }
+}
