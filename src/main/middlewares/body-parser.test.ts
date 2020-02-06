926f9b533885306526710940403fce6b687e338a
Luiz Pires <luiz@kw13.com.br>
2020-02-05 21:06:07 -0300

    chore: ensure requests parse body as json

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -4,11 +4,12 @@ import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
   test('Sould parser body as json', async () => {
-    app.post('test_body_parser', (req, res) => {
+    app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
     await request(app)
       .post('/test_body_parser')
       .send({ name: 'Kandrus' })
+      .expect({ name: 'Kandrus' })
   })
 })
