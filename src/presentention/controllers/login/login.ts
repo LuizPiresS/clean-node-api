8cbe2504e83923c14eb2ab93401a10aac5582a3e
Luiz Pires <luiz@kw13.com.br>
2020-02-09 04:38:46 -0300

    refactor: remove code duplication

--- src/presentention/controllers/login/login.ts
+++ src/presentention/controllers/login/login.ts
@@ -9,7 +9,6 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
-
     if (!email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
     }
