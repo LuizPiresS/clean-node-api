1bbefa921fbdff35f6bb244492972b92074216c3
Luiz Pires <luiz@kw13.com.br>
2020-02-11 04:06:18 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/presentention/controllers/signup/signup-protocols.ts
+++ src/presentention/controllers/signup/signup-protocols.ts
@@ -2,3 +2,4 @@ export * from '../../protocols' // protocoloes genericos
 export * from '../../protocols/email-validator' // Protocolos especificos
 export * from '../../../domain/usecases/add-account' // protocolos externos
 export * from '../../../domain/models/account' // protocolos externos
+export * from '../../helpers/validators/validation'
--- src/presentention/controllers/signup/signup.ts
+++ src/presentention/controllers/signup/signup.ts
@@ -1,16 +1,20 @@
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { Validation } from '../../helpers/validators/validation'
 import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount } from './signup-protocols' // Protocolos genericos
 export class SignUpController implements Controller {
+  private readonly validation: Validation
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
+    this.validation = validation
     this.emailValidator = emailValidator
     this.addAccount = addAccount
   }
 
   public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
