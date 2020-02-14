d1b3c9d9de5a879f301dd246868a0c453ccbd8a6
Luiz Pires <luiz@kw13.com.br>
2020-02-14 10:07:03 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 1313
+  port: process.env.PORT || 1313,
+  jwtSecret: process.env.JWT_SECRET || 'chavesecretajwt'
 }
